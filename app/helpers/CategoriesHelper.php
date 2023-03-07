bd6a969f29e2fa794352ee68947e32ccf00faca7
Cools1337 <cools1337@mail.ru>
2023-03-07 22:54:33 +0300

    Create controllers and helper

---
+++ app/helpers/CategoriesHelper.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Helpers;
+
+class CategoriesHelper
+{
+    public static function getCategories()
+    {
+        return [
+            'Компьютеры',
+            'Телефоны',
+            'Телевизоры',
+            'Фотоаппараты',
+            'Аудиотехника',
+            'Кухонные приборы',
+            'Стиральные машины',
+            'Холодильники',
+            'Мебель',
+            'Одежда',
+        ];
+    }
+}
