29567a85f0620c0c50ed2e9aafc3e4c607f01f0f
Cools1337 <cools1337@mail.ru>
2023-03-09 22:41:54 +0300

    edit job and add schedule

--- app/Jobs/UpdatePopularCategoriesCount.php
+++ app/Jobs/UpdatePopularCategoriesCount.php
@@ -43,35 +43,5 @@ class UpdatePopularCategoriesCount implements ShouldQueue
                  ['reviews_count' => $category->reviews_count]
              );
          });
-
-        
-        // $reviewsCountByCategory = DB::table('categories')
-        // ->select('categories.id', DB::raw('COUNT(reviews.id) as reviews_count'))
-        // ->leftJoin('products_categories', 'categories.id', '=', 'products_categories.category_id')
-        // ->leftJoin('products', 'products_categories.product_id', '=', 'products.id')
-        // ->leftJoin('reviews', 'products.id', '=', 'reviews.product_id')
-        // ->groupBy('categories.id')
-        // ->get();
-            
-        // foreach ($reviewsCountByCategory as $reviewsCount) {
-        //     Category::where('id', $reviewsCount->id)
-        //         ->update(['reviews_count' => $reviewsCount->reviews_count]);
-        // }
-    
-    
-
-        // $categories = Category::all();
-
-        //  foreach ($categories as $category) {
-        //    $reviewsCount = Review::whereHas('product', function ($query) use ($category) {
-        //           $query->whereHas('categories', function ($query) use ($category) {
-        //               $query->where('categories.id', $category->id);
-        //           });
-        //       })->count();
-        //       Category::updateOrCreate(
-        //           ['id' => $category->id],
-        //           ['reviews_count' => $reviewsCount]
-        //       );
-        //  }
     }
 }
